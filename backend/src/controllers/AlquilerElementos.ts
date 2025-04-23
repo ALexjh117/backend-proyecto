89b5aed1494bc3e5cd92f95a07c3eb4050030883
Alexjh117 <alexchaguendo01@gmail.com>
2025-04-23 11:28:57 -0500

    3 crud

--- backend/src/controllers/AlquilerElementos.ts
+++
@@ -1,7 +0,0 @@
-import type { Request , Response } from "express";
-
-export class AlquilerElementosControllers{
-    static getAll =async (req:Request , res:Response) =>{
-        console.log('Desde Get /api/ hola mundo 223232')
-    }
-}
--- backend/src/routes/ActividadRouter.ts
+++ backend/src/routes/ActividadRouter.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 import { body,param } from 'express-validator';
-import { ActividadControllers } from '../controllers/ActividadController';
+import {ActividadControllers} from '../controllers/ActividadController';
 import {handleInputErrors} from '../middleware/validation'
 import {validateActividadBody,validateIdActividad, validateIdActividadYaExiste,} from '../middleware/Actividad';
 
