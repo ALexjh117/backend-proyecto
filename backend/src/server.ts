959b6f6ed0c8d8251f180d31407442c5a5545f52
Victor <victormanuelmosqueraconejo@gmail.com>
2025-04-23 12:04:54 -0500

    Rutas de API Usuario y Actividades

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -4,6 +4,8 @@ import morgan from 'morgan';
 import { db } from './config/db';
 import ActividadRouter from './routes/ActividadRouter'
 import AlquilerElementosRouter from './routes/AlquilerElementosRouter';
+import UsuarioRoute from './routes/UsuarioRoute';
+import GestionEventoRoute from './routes/GestionEventoRoute';
 
 
 async function connectDB() {
@@ -31,6 +33,8 @@ app.use(morgan('dev'));
 app.use(express.json());
 app.use('/api/actividad', ActividadRouter);
 app.use('/api/alquilerelementos', AlquilerElementosRouter);
+app.use("/api/usuario", UsuarioRoute)
+app.use("/api/gestionevento", GestionEventoRoute)
 
 
 
